fc42d6de04d7c8d7a778ecb54d2e5a766dc555e3
daum0103 choi <daumi0103@gmail.com>
2025-03-31 03:23:39 +0000

    if문 활용해서 아스키 코드 그림 출력

--- myif.py
+++ myif.py
@@ -1,4 +1,35 @@
 # 아스키 코드 그림 출력하기
+def print_cat():
+    cat = [
+        " /\\_/\\  ",
+        "( o.o ) ",
+        " > ^ <  "
+    ]
+    
+    for line in cat:
+        print(line)
+
+def print_dog():
+    dog = [
+        " / \\__",
+        "(    @\\___",
+        " /         O",
+        "/   (_____/",
+        "/_____/   U"
+    ]
+
+    for line in dog:
+        print(line)
+
+def print_rabbit():
+    rabbit = [
+        "  (\\_/)",
+        "  (o o)",
+        "  ( > )"
+    ]
+    
+    for line in rabbit:
+        print(line)
 
 print("그림 출력 프로그램")
 print("=====================")
@@ -10,12 +41,15 @@ n = int(input("선택: "))
 # 만약에 1을 입력하면 1번에 캐릭터 출력
 if n == 1:
     print("고양이 그림")
+     print_cat()
 # 만약에 2를 입력하면 2번 캐릭터 출력
 elif n == 2:
     print("강아지")
+     print_dog()
     # 3을 입력하면 3번 캐릭터 출력
 elif n == 3:
     print("토 끼")
+    print_rabbit()
 # 잘못 입력하면 잘못 입력했다고 출력
 else 
     print("잘못 입력")
